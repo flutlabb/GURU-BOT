1faf226e9d9c82027ef8a65de1ee33e82ce53792
youngtiger1 <103368810+youngtiger1@users.noreply.github.com>
2024-01-05 03:37:49 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,16 +7,16 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['255788715410', 'nikki', true],
-  ['6281255369012', 'Ash', true], 
+  ['255673531233', 'nikki', true],
+  ['6281255369012', 'Nikki', true], 
   [''] 
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['255788715410','32460248586'] 
-global.prems = ['255788715410', '32460248586', '919398758484']
-global.allowed = ['255788715410', '32460248586', '19152999993']
+global.mods = ['255673531233','32460248586'] 
+global.prems = ['255673531233', '32460248586', '919398758484']
+global.allowed = ['255673531233', '32460248586', '19152999993']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -50,10 +50,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
-global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
+global.botname = 'Nikki.AI'
+global.premium = 'false'
+global.packname = 'nikki.AI' 
+global.author = '@luxeenet' 
 global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BFfD1C0mTDDDfVdKPkxRAA'
